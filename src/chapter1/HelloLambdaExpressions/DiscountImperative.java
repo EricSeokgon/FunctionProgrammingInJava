b639334251bc81e70ce2b4420b8a6084580a8fd8
EricSeokgon <leesk55@gmail.com>
2021-06-28 18:03:46 +0900

    The Old Way

---
+++ src/chapter1/HelloLambdaExpressions/DiscountImperative.java
@@ -0,0 +1,24 @@
+package chapter1.HelloLambdaExpressions;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class DiscountImperative {
+    public static void main(String[] args) {
+        final List<BigDecimal> prices = Arrays.asList(
+                new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+                new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+                new BigDecimal("45"), new BigDecimal("12")
+        );
+
+        BigDecimal totalOfDiscountePrices = BigDecimal.ZERO;
+
+        for (BigDecimal price : prices) {
+            if (price.compareTo(BigDecimal.valueOf(20)) > 0) {
+                totalOfDiscountePrices = totalOfDiscountePrices.add(price.multiply(BigDecimal.valueOf(0.9)));
+            }
+            System.out.println("Total of discounted prices: " + totalOfDiscountePrices);
+        }
+    }
+}
