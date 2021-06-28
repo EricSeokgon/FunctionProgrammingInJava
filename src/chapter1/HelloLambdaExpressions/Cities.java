c4aafb913f215e70f58d4aab323cf12109f58f3b
EricSeokgon <leesk55@gmail.com>
2021-06-28 15:19:36 +0900

    A Better Way

--- src/chapter1/HelloLambdaExpressions/Cities.java
+++ src/chapter1/HelloLambdaExpressions/Cities.java
@@ -1,16 +1,29 @@
 package chapter1.HelloLambdaExpressions;
 
-public class Cities {
-    public static void main(String[] args) {
-        boolean found = false;
-        String[] cities ={"Chicago"};
+import java.util.Arrays;
+import java.util.List;
 
+public class Cities {
+    public static void findChicagoImperative(final List<String> cities) {
+        boolean found = false;
         for (String city : cities) {
             if (city.equals("Chicago")) {
                 found = true;
                 break;
             }
         }
+
         System.out.println("Found chicago?:" + found);
+    }
+
+    public static void findChicagoDeclarative(final List<String> cities) {
+        System.out.println("Found chicago?:" + cities.contains("Chicago"));
+    }
+
+    public static void main(final String[] args) {
+        List<String> cities = Arrays.asList("Albany", "Boulder", "Chicago", "Denver", "Eugene");
+
+        findChicagoImperative(cities);
+        findChicagoDeclarative(cities);
     }
 }
