27613feae61d737358c1e34458f0b1d76d26ab4d
EricSeokgon <leesk55@gmail.com>
2021-06-28 18:16:35 +0900

    Iterating through a List

---
+++ src/chpter2/UsingCollections/iterating.java
@@ -0,0 +1,14 @@
+package chpter2.UsingCollections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class iterating {
+    public static void main(String[] args) {
+        final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Scott");
+
+        for (int i = 0; i < friends.size(); i++) {
+            System.out.println(friends.get(i));
+        }
+    }
+}
