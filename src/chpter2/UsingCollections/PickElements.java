f22d36d4d2df40d7ca90e855f0dd3cdb5fb82f7f
EricSeokgon <leesk55@gmail.com>
2021-07-07 18:06:13 +0900

    Let’s refactor this code to use the filter() method, and see how it changes things

--- src/chpter2/UsingCollections/PickElements.java
+++ src/chpter2/UsingCollections/PickElements.java
@@ -1,16 +1,23 @@
 package chpter2.UsingCollections;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PickElements {
     final private static List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Scott");
 
     public static void main(String[] args) {
-        final List<String> startsWithN = new ArrayList<String>();
+        /*final List<String> startsWithN = new ArrayList<String>();
         for (String name : friends) {
+            if(name.startsWithN("N")){
             startsWithN.add(name);
-        }
+            }
+        }*/
+
+        final List<String> startsWithN =
+                friends.stream()
+                        .filter(name -> name.startsWith("N"))
+                        .collect(Collectors.toList());
     }
 }
